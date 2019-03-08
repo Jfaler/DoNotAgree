452bc33d90d62842f6c9e0ee9dc3c71c0456f7c6
Justin Faler <Jstnfaler@gmail.com>
2019-03-08 11:58:35 -0800

    Update dna.js

--- dna.js
+++ dna.js
@@ -1,6 +1,6 @@
 /*
 Author: 🎨 Justin Rayfield Faler
 Date & time: 📅  3/8/2019 11:52AM Pacific Time
-Description: ❎ Help contribute to the library of  websites to disagree with.
+Description: ❎ Help contribute to the library of websites to disagree with.
 */
 document.getElementById("agree").innerHTML = "I do not agree!";
